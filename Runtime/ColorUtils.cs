181df5a72a981d2403b13f6e1dd52cf170d2d948
Sebastian Adamowski <s.adamowski94@gmail.com>
2023-09-17 22:50:15 +0200

    Added functionality to calculation color from hex and hex from color

---
+++ Runtime/ColorUtils.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace PolySnake.Core
+{
+    public static class ColorUtils
+    {
+        public static string colorToHex(Color32 color)
+        {
+            string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+            return hex;
+        }
+
+        public static Color hexToColor(string hex)
+        {
+            hex = hex.Replace("0x", "");
+            hex = hex.Replace("#", "");
+            byte a = 255;
+            byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+            byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+            byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+
+            if (hex.Length == 8)
+            {
+                a = byte.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber);
+            }
+
+            return new Color32(r, g, b, a);
+        }
+    }
+}
